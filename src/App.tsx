bf0b856078e73f9026929ca5b0e7b17050acbae4
bgragavendiran <bgragavendiran@gmail.com>
2022-12-03 17:13:03 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import Button from '@mui/material/Button';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import Stack from '@mui/material/Stack';
+declare global {
+  interface Window {
+      myObject: any;
+  }
+}
 
 function App() {
+  var remainingXats;
+  const openInNewTab=(url:String)=> {
+    (window as any).myObject.open(url, '_blank').focus();
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Total Earnings: <code>remainingXats</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Total Ads Consumed: <code>61k</code>
+        </p>
+        <p>
+          Highest Ad Streak: <code>3</code>
+        </p>
+        <Button onClick={()=>{openInNewTab('www.google.com');}} variant="contained" endIcon={<OpenInNewIcon />} fullWidth>Dashboard</Button>
+    
       </header>
     </div>
   );
